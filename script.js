36b95eba34707c500a043ee203f99906eb7edace
Soubhik <soubhikpramanik10@gmail.com>
2024-11-13 10:22:27 +0530

    Add demo sms feature

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const chatWindow = document.querySelector('.chat_window');
 const closeIcon = document.querySelector('.close_icon');
 const chatIcon = document.querySelector('.chat_icon');
+let input = document.getElementById('inputField');
+const sendBtn = document.querySelector('.send_btn')
+const chatBox = document.querySelector('.chat_sms');
 
 
 chatIcon.addEventListener('click', ()=>{
@@ -13,4 +16,16 @@ closeIcon.addEventListener('click', () => {
     // chatWindow.style.display = "none";
     chatWindow.classList.remove('show');
     chatIcon.style.display = "flex";
+})
+
+sendBtn.addEventListener('click', () => {
+    let inputValue = input.value;
+    chatBox.innerHTML += `
+                <div class="right">
+                    <p class="text">${inputValue}</p>
+                    <div class="circle">S</div>
+                </div> 
+    `
+    input.value = ""
+        
 })
